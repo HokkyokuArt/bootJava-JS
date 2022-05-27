828f6cde617e2eb1ee5dd434017373ddce62491e
Artico <jeanartico13@hotmail.com>
2022-05-27 00:50:18 -0300

    Login feito com for (questão de honra)

--- desafios/login/script/script.js
+++ desafios/login/script/script.js
@@ -1,56 +1,48 @@
-// window.onload = function () {
-    const user = document.querySelector('#user')
-    const pws = document.querySelector('#pws')
-    const btn = document.querySelector('#btn')
+const user = document.querySelector('#user')
+const pws = document.querySelector('#pws')
+const btn = document.querySelector('#btn')
 
-    btn.addEventListener('click', (e) => {
-        e.preventDefault()
-        fazer_requisicao()
-    })
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    fazer_requisicao()
+})
 
+let fazer_requisicao = async function () {
+    let usuarios = `./script/usuario.json`
+    let dadosFetch = await fetch(usuarios)
+    let dadosJson = await dadosFetch.json();
 
-
-    let fazer_requisicao = async function () {
-        let usuarios = `./script/usuario.json`
-        let dadosFetch = await fetch(usuarios)
-        let dadosJson = await dadosFetch.json();
-    
-        verificar_Credenciais(dadosJson.users);
-
+    if (verificar_Credenciais(dadosJson.users)) {
+        window.location.href = `../panel/panel.html`
+        user.value = ''
+        pws.value = ''
+    } else {
+        alert(`Usuario ou senha incorreta`)
     }
+}
 
-    function verificar_Credenciais(dadosJson) {
-
-        
-        const findUser = dadosJson.find((item) => {
-            return item.user === user.value && item.pws === pws.value;
-        })
-
-        if(findUser){
-            user.value = ''
-            pws.value = ''
-            window.location.href = `../panel/panel.html`
-            
-            
-        } else {
-            alert(`Usuario ou senha incorreta`)
+function verificar_Credenciais(dadosJson) {
+    for (let User of dadosJson) {
+        if (User.user === user.value && User.pws === pws.value) {
+            return true
         }
-        
-        
-
     }
+}
 
 
+//_________________________________ Copiado :( _________________________________//
+
+    // const findUser = dadosJson.find((item) => {
+    //     return item.user === user.value && item.pws === pws.value;
+    // })
+
+    // if(findUser){
+    //     window.location.href = `../panel/panel.html`
+    //     user.value = ''
+    //     pws.value = ''
+    //    
 
 
-
-
-
-
-
-
-
-
-
-
-// } // Fim do onload
+    // } else {
+    //     alert(`Usuario ou senha incorreta`)
+    // }
